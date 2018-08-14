0ff416d126728f9b911abab79123f02f3a006478
Dan Mace and Hassan Al-khalifah <dan@>
2018-08-14 14:02:15 -0700

    make primez

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var multiplesArray = function(mult, stop) {
   var array = [];
-  for (var n=1; mult*n <= stop; n++ ) {
+  for (var n=2; mult*n <= stop; n++ ) {
     array.push(mult*n);
+    console.log(mult*n);
   }
   return array;
 }
@@ -12,7 +13,7 @@ $(function (){
   var multiples = [];
   var start = 2;
   var result = [];
-  var input = prompt("Enter a maximum number to compute primes: ");
+  var input = parseInt(prompt("Enter a maximum number to compute primes: "));
 
   for( var i = start; i <= input; i++ ) {
     numberList.push(i);
@@ -20,20 +21,35 @@ $(function (){
   console.log(numberList);
 
   var last = numberList[numberList.length-1];
-  var init = numberList[0];
-  var badList = multiplesArray(init, last);
-  numberList.forEach(function(number, index) {
+  var init;
+
+
+
+  var tossBadBoys = function () {
+    init = numberList[0];
+    result.push(numberList[0]);
+    numberList.forEach(function(number, index) {
+    console.log(number, index);
+    var badList = multiplesArray(init, last);
     badList.forEach(function(badNumber) {
+      console.log(number, badNumber, "this");
       if (number == badNumber) {
-        numberList.splice(numberList[index], 1);
-      }
+        console.log(numberList);
+        numberList.splice(index, 1);
+        console.log(numberList);
+        }
+      })
     })
-  })
+    numberList.shift();
+  }
+
+  for ( var n = 0; n < last-1; n++ ) {
+    tossBadBoys();
+  }
+
+  $("#result").text(result.join(" "));
 
 
-  console.log(result);
-
-  console.log(numberList);
   //
   // numberList.forEach(function(number) {
   //   if ()
