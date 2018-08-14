081f06e0e64bd7e1d5f85f95cb89f7566f60a6b5
Dan Mace and Hassan Al-khalifah <dan@>
2018-08-14 15:26:58 -0700

    made better

--- js/scripts.js
+++ js/scripts.js
@@ -2,51 +2,40 @@ var multiplesArray = function(mult, stop) {
   var array = [];
   for (var n=2; mult*n <= stop; n++ ) {
     array.push(mult*n);
-    console.log(mult*n);
+    console.log(mult);
   }
   return array;
 }
 
+var tossBadBoys = function (array) {
+  var start = array[0];
+  var end = array[array.length-1];
+  var badList = multiplesArray(start, end);
+
+  array.forEach(function(number, index) {
+    badList.forEach(function(badNumber) {
+      if (number == badNumber) {
+        array.splice(index, 1);
+      }
+    });
+  });
+  array.shift();
+
+  return start;
+}
+
 $(function (){
 
-  var numberList = [];
-  var multiples = [];
-  var start = 2;
   var result = [];
   var input = parseInt(prompt("Enter a maximum number to compute primes: "));
 
-  for( var i = start; i <= input; i++ ) {
-    numberList.push(i);
+  var numberList = multiplesArray(1, input);
+
+
+  for (var n = 0; n < Math.sqrt(input); n++) {
+    result.push(tossBadBoys(numberList));
   }
-  console.log(numberList);
-
-  var last = numberList[numberList.length-1];
-  var init;
-
-
-
-  var tossBadBoys = function () {
-    init = numberList[0];
-    result.push(numberList[0]);
-    numberList.forEach(function(number, index) {
-    console.log(number, index);
-    var badList = multiplesArray(init, last);
-    badList.forEach(function(badNumber) {
-      console.log(number, badNumber, "this");
-      if (number == badNumber) {
-        console.log(numberList);
-        numberList.splice(index, 1);
-        console.log(numberList);
-        }
-      })
-    })
-    numberList.shift();
-  }
-
-  for ( var n = 0; n < last-1; n++ ) {
-    tossBadBoys();
-  }
-
+  result = result.concat(numberList);
   $("#result").text(result.join(" "));
 
 
